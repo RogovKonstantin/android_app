d80f7635d95073eda9fef5c49c42e2d6c93dbab4
RogovKonstantin <102481199+RogovKonstantin@users.noreply.github.com>
2024-09-11 17:36:59 +0300

    Update RetroFitInstance.kt

--- app/src/main/java/utils/api/RetroFitInstance.kt
+++ app/src/main/java/utils/api/RetroFitInstance.kt
@@ -6,14 +6,10 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitInstance {
     private const val BASE_URL = "https://thronesapi.com/api/v2/"
 
-    private val retrofit: Retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
+    val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
-    val api: ApiService by lazy {
-        retrofit.create(ApiService::class.java)
-    }
+    val api: ApiService = retrofit.create(ApiService::class.java)
 }
