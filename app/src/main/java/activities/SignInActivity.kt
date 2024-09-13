2aa7590ecaaa8b4f53dbf3e3dafbd9b590c8692a
d50776310@gmail.com <d50776310@gmail.com>
2024-09-13 15:41:12 +0300

    fix flow

--- app/src/main/java/activities/SignInActivity.kt
+++ app/src/main/java/activities/SignInActivity.kt
@@ -6,12 +6,13 @@ import com.google.android.material.button.MaterialButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
+import android.widget.TextView
 import com.example.andr_dev_application.R
 
 class SignInActivity : AppCompatActivity() {
 
-    private val validEmail = "test@example.com"
-    private val validPassword = "password123"
+    private val validEmail = "q"
+    private val validPassword = "q"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,11 +21,11 @@ class SignInActivity : AppCompatActivity() {
         val emailInput: EditText = findViewById(R.id.email_input)
         val passwordInput: EditText = findViewById(R.id.password_input)
         val loginButton: MaterialButton = findViewById(R.id.login_button)
+        val signUpLink: TextView = findViewById(R.id.sign_up_link)
 
         loginButton.setOnClickListener {
             val enteredEmail = emailInput.text.toString().trim()
             val enteredPassword = passwordInput.text.toString().trim()
-
 
             if (enteredEmail.isEmpty() || enteredPassword.isEmpty()) {
                 Toast.makeText(this, "Enter both email and password", Toast.LENGTH_SHORT).show()
@@ -34,11 +35,17 @@ class SignInActivity : AppCompatActivity() {
             if (enteredEmail == validEmail && enteredPassword == validPassword) {
                 Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this, MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                 startActivity(intent)
                 finish()
             } else {
                 Toast.makeText(this, "Invalid Email or Password", Toast.LENGTH_SHORT).show()
             }
+        }
+
+        signUpLink.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/activities/SignUpActivity.kt
@@ -0,0 +1,40 @@
+package activities
+
+import android.os.Bundle
+import android.widget.EditText
+import com.google.android.material.button.MaterialButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import com.example.andr_dev_application.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        val emailInput: EditText = findViewById(R.id.email_input)
+        val usernameInput: EditText = findViewById(R.id.username_input)
+        val passwordInput: EditText = findViewById(R.id.password_input)
+        val registerButton: MaterialButton = findViewById(R.id.register_button)
+
+        registerButton.setOnClickListener {
+            val enteredEmail = emailInput.text.toString().trim()
+            val enteredUsername = usernameInput.text.toString().trim()
+            val enteredPassword = passwordInput.text.toString().trim()
+
+            if (enteredEmail.isEmpty() || enteredUsername.isEmpty() || enteredPassword.isEmpty()) {
+                Toast.makeText(this, "Enter email, username, and password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            else{
+            Toast.makeText(this, "Registration Successful", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            finish()}
+        }
+    }
+}
