10d402bb6d8a40e0a5d7cf1a2035c1bdc9addf88
d50776310@gmail.com <d50776310@gmail.com>
2024-10-29 12:16:13 +0300

    cleanup

--- app/src/main/java/activities/MainActivity.kt
+++ app/src/main/java/activities/MainActivity.kt
@@ -2,24 +2,17 @@ package activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.andr_dev_application.R
-import kotlinx.coroutines.launch
-import utils.ThemePreferences
+
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
-    private lateinit var themePreferences: ThemePreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        themePreferences = ThemePreferences(this)
-
-
-        applyTheme()
         setContentView(R.layout.activity_main)
 
         val navHostFragment = supportFragmentManager
@@ -28,15 +21,6 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun applyTheme() {
-        lifecycleScope.launch {
-            themePreferences.isDarkMode.collect { isDarkMode ->
-                AppCompatDelegate.setDefaultNightMode(
-                    if (isDarkMode) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
-                )
-            }
-        }
-    }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
